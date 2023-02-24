493e919681a091269f3b370e41e8613443295b02
Amisszsiy <nyu_miss@hotmail.com>
2023-02-24 21:58:28 +0700

    Added clock UI

---
+++ NEMS/Controllers/ClockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NEMS.Controllers
+{
+    public class ClockController : Controller
+    {
+        public IActionResult Clock()
+        {
+            return View();
+        }
+    }
+}
--- NEMS/Controllers/HomeController.cs
+++ NEMS/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NEMS.Data;
 using NEMS.Models;
 using System.Diagnostics;
 
@@ -7,14 +9,26 @@ namespace NEMS.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ApplicationUser user { get; private set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _db = db;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            System.Security.Claims.ClaimsPrincipal currentUser = User;
+            var id = _userManager.GetUserId(currentUser);
+            user = _db.Users.Find(id);
+            if (user != null)
+            {
+                ViewData["userFirstName"] = user.FirstName;
+            }
             return View();
         }
 
